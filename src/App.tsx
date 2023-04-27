cb96ca95009675b8a9f8abe6bd1b196513f0d92c
arjunarj <nagarjunan66@gmail.com>
2023-04-27 22:42:10 +0530

    Add styles and result message

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ import axios from 'axios'
 
 
 function App() {
-  const [result, setResult] = useState(null)
+  const [result, setResult] = useState("")
   const { register, handleSubmit } = useForm();
 
+
+  const resultMessage = (result: string) => {
+    if (result == "positive"){
+      return "Positive 😊"
+    } else if (result == "negative") {
+      return "Negative 😕"
+    } else if (result == "neutral") {
+      return "Neutral 😐"
+    } else {
+      return "Error occured! Please try again"
+    }
+  }
+
   const fetchResult = handleSubmit((formData) => {
-    axios.post("http://192.168.11.8:8000/predict", formData)
+    axios.post("http://localhost:8000/predict", formData)
       .then(response => {
         console.log(response.data);
-        setResult(response.data)
+        setResult(resultMessage(response.data))
       })
       .catch(error => {
         console.log(error);
+        setResult("Error occured! Please try again")
       });
   });
 
@@ -24,8 +38,10 @@ function App() {
     <>
       <h1>Enter a comment</h1>
       <form onSubmit={fetchResult}>
-        <input {...register("comment")} type='text'/>
-        <input type="submit"/>
+        <div className='row'>
+          <input className='textfield' {...register("comment")} type='text' placeholder='Type any comment'/>
+          <input type="submit" className='button'/>
+        </div>
       </form>
       <p>{result}</p>
     </>
